0869bfa2dd1c247606bf2f5d29695c1c5dd5d58f
Hariprasath A U <160257595+Hari242005prasath@users.noreply.github.com>
2024-09-26 16:21:06 +0530

    Add files via upload

---
+++ Coding-club/index.js
@@ -0,0 +1,16 @@
+const imageUpload = document.getElementById('imageUpload');
+        const imagePreview = document.getElementById('imagePreview');
+
+        imageUpload.addEventListener('change', function() {
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function(event) {
+                    imagePreview.setAttribute('src', event.target.result);
+                    imagePreview.style.display = 'block';
+                };
+
+                reader.readAsDataURL(file);
+            }
+        });
